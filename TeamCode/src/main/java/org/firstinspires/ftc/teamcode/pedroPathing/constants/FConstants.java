584dc52829601c88e070f4378c34f9b4645fe4dc
ehopson-cmyk <ehopson@greenwoodjax.org>
2025-09-03 13:14:26 -0400

    Ported over to the new version

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
@@ -6,48 +6,59 @@ import com.pedropathing.util.CustomFilteredPIDFCoefficients;
 import com.pedropathing.util.CustomPIDFCoefficients;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-public class FConstants {
-    static {
-        //needed to change to pinpoint localizer
-        FollowerConstants.localizers = Localizers.THREE_WHEEL;
+public class Constants {
 
-        FollowerConstants.leftFrontMotorName = "leftFront";
-        FollowerConstants.leftRearMotorName = "leftRear";
-        FollowerConstants.rightFrontMotorName = "rightFront";
-        FollowerConstants.rightRearMotorName = "rightRear";
+  public static FollowerConstants followerConstants = new FollowerConstants()
+    .mass(13)
+    .forwardZeroPowerAcceleration(-41.278)
+    .lateralZeroPowerAcceleration(-59.7819)
+    .useSecondaryTranslationalPIDF(false)
+    .useSecondaryHeadingPIDF(false)
+    .useSecondaryDrivePIDF(false)
+    .centripetalScaling(0.0005)
+    .translationalPIDFCoefficients(new PIDFCoefficients(0.1, 0, 0.01, 0))
+    .headingPIDFCoefficients(new PIDFCoefficients(2, 0, 0.1, 0))
+    .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.1, 0, 0, 0.6, 0));
 
-        FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
-        FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
-        FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
-        FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+  public static MecanumConstants driveConstants = new MecanumConstants()
+    .leftFrontMotorName("leftFront")
+    .leftRearMotorName("leftRear")
+    .rightFrontMotorName("rightFront")
+    .rightRearMotorName("rightRear")
+    .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+    .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+    .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+    .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+    .xVelocity(57.8741)
+    .yVelocity(52.295);
 
-        FollowerConstants.mass = 13;
+  public static ThreeWheelConstants localizerConstants =
+    new ThreeWheelConstants()
+      .forwardTicksToInches(.001989436789)
+      .strafeTicksToInches(.001989436789)
+      .turnTicksToInches(.001989436789)
+      .leftPodY(1)
+      .rightPodY(-1)
+      .strafePodX(-2.5)
+      .leftEncoder_HardwareMapName("leftFront")
+      .rightEncoder_HardwareMapName("rightRear")
+      .strafeEncoder_HardwareMapName("rightFront")
+      .leftEncoderDirection(Encoder.REVERSE)
+      .rightEncoderDirection(Encoder.REVERSE)
+      .strafeEncoderDirection(Encoder.FORWARD);
 
-        FollowerConstants.xMovement = 57.8741;
-        FollowerConstants.yMovement = 52.295;
+  public static PathConstraints pathConstraints = new PathConstraints(
+    0.995,
+    500,
+    1,
+    1
+  );
 
-        FollowerConstants.forwardZeroPowerAcceleration = -41.278;
-        FollowerConstants.lateralZeroPowerAcceleration = -59.7819;
-
-        FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0);
-        FollowerConstants.useSecondaryTranslationalPID = false;
-        FollowerConstants.secondaryTranslationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID
-
-        FollowerConstants.headingPIDFCoefficients.setCoefficients(2,0,0.1,0);
-        FollowerConstants.useSecondaryHeadingPID = false;
-        FollowerConstants.secondaryHeadingPIDFCoefficients.setCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
-
-        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0);
-        FollowerConstants.useSecondaryDrivePID = false;
-        FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
-
-        FollowerConstants.zeroPowerAccelerationMultiplier = 4;
-        FollowerConstants.centripetalScaling = 0.0005;
-
-        FollowerConstants.pathEndTimeoutConstraint = 500;
-        FollowerConstants.pathEndTValueConstraint = 0.995;
-        FollowerConstants.pathEndVelocityConstraint = 0.1;
-        FollowerConstants.pathEndTranslationalConstraint = 0.1;
-        FollowerConstants.pathEndHeadingConstraint = 0.007;
-    }
+  public static Follower createFollower(HardwareMap hardwareMap) {
+    return new FollowerBuilder(followerConstants, hardwareMap)
+      .mecanumDrivetrain(driveConstants)
+      .threeWheelLocalizer(localizerConstants)
+      .pathConstraints(pathConstraints)
+      .build();
+  }
 }
