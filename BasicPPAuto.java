a4a89281293d695e05aca73d59a12abc2f05d7f2
ehopson-cmyk <ehopson@greenwoodjax.org>
2026-02-24 12:57:04 -0500

    Create BasicPPAuto.java

---
+++ BasicPPAuto.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.TelemetryManager;
+import com.bylazar.telemetry.PanelsTelemetry;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.geometry.Pose;
+
+@Autonomous(name = "Pedro Pathing Autonomous", group = "Autonomous")
+@Configurable // Panels
+public class BasicPPAuto extends OpMode {
+  private TelemetryManager panelsTelemetry; // Panels Telemetry instance
+  public Follower follower; // Pedro Pathing follower instance
+  private int pathState; // Current autonomous path state (state machine)
+  private Paths paths; // Paths defined in the Paths class
+
+  @Override
+  public void init() {
+    panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
+
+    follower = Constants.createFollower(hardwareMap);
+    follower.setStartingPose(new Pose(72, 8, Math.toRadians(90)));
+
+    paths = new Paths(follower); // Build paths
+
+    panelsTelemetry.debug("Status", "Initialized");
+    panelsTelemetry.update(telemetry);
+  }
+
+  @Override
+  public void loop() {
+    follower.update(); // Update Pedro Pathing
+    pathState = autonomousPathUpdate(); // Update autonomous state machine
+
+    // Log values to Panels and Driver Station
+    panelsTelemetry.debug("Path State", pathState);
+    panelsTelemetry.debug("X", follower.getPose().getX());
+    panelsTelemetry.debug("Y", follower.getPose().getY());
+    panelsTelemetry.debug("Heading", follower.getPose().getHeading());
+    panelsTelemetry.update(telemetry);
+  }
+
+  public static class Paths {
+    public PathChain Path1;
+    public PathChain Path2;
+
+    public Paths(Follower follower) {
+      Path1 = follower.pathBuilder()
+          .addPath(
+            new BezierLine(
+              new Pose(56.000, 8.000),
+            new Pose(56.000, 87.238)
+            )
+          )
+          .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(135))
+          .build();
+
+      Path2 = follower.pathBuilder()
+          .addPath(
+            new BezierLine(
+              new Pose(56.000, 87.238),
+            new Pose(105.307, 33.479)
+            )
+          )
+          .setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(90))
+          .build();
+    }
+  }
+
+  public int autonomousPathUpdate() {
+    // Add your state machine Here
+    // Access paths with paths.pathName
+    // Refer to the Pedro Pathing Docs (Auto Example) for an example state machine
+    return 0;
+  }
+}
